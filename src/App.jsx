77da950e30aae038f575c6da70c14734483e6f68
Osorio3408 <yuliamwow@gmail.com>
2022-09-13 17:18:43 -0500

    se agrega contacto

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Nosotros from './pages/Nosotros'
+import Servicios from './pages/Servicios'
+import Inicio from './pages/Inicio'
+import Contacto from './pages/Contacto'
+
+
+
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      
+      <Router>
+      <Navbar/>
+        <Routes>
+            <Route path='/' element={<Inicio/>} />
+            <Route path='/Nosotros' element={<Nosotros/>} />
+            <Route path='/Servicios' element={<Servicios/>} />
+            <Route path='/Contacto' element={<Contacto/>} />
+        </Routes>
+      <Footer/>
+      </Router>
+
+
+
+
+
+    </div>
+  )
+}
+
+
+export default App
+
+
