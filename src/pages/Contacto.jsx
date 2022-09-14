88a43c54ff4ee4d26a14fbcc54728081b020ad29
Osorio3408 <yuliamwow@gmail.com>
2022-09-14 16:49:37 -0500

    se hizo unos cambios en el css

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -51,7 +51,7 @@ const Contacto = () =>{
           <div className="contacto">
           <h3>Mensaje :</h3>
               <div className="">
-                <textarea name="mensaje" id="" cols="109" rows="5" placeholder="Ingresa tu mensaje..."></textarea>
+                <textarea name="mensaje" id="" cols="87" rows="5" placeholder="Ingresa tu mensaje..."></textarea>
               </div>
           </div>
 
@@ -60,7 +60,7 @@ const Contacto = () =>{
           </div>
 
         <div className="mapa">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d3977.2931409960984!2d-75.67083553469256!3d4.541159194283394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d4.5416210999999995!2d-75.66856709999999!4m5!1s0x8e38f5aaa70689d1%3A0x46074d66ee20f3a2!2sSENA%20Centro%20de%20comercio%20y%20turismo%2C%20Carrera%2018%2C%20Armenia%2C%20Quind%C3%ADo!3m2!1d4.5410284!2d-75.6684646!5e0!3m2!1ses!2sco!4v1663101122938!5m2!1ses!2sco" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d3977.2931409960984!2d-75.67083553469256!3d4.541159194283394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d4.5416210999999995!2d-75.66856709999999!4m5!1s0x8e38f5aaa70689d1%3A0x46074d66ee20f3a2!2sSENA%20Centro%20de%20comercio%20y%20turismo%2C%20Carrera%2018%2C%20Armenia%2C%20Quind%C3%ADo!3m2!1d4.5410284!2d-75.6684646!5e0!3m2!1ses!2sco!4v1663101122938!5m2!1ses!2sco" width="450" height="350"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
     
